82d8dd4c15fa480993a7e882588b2187f397ba0c
SW_Creeperking <sw.creeperking@gmail.com>
2020-12-04 16:20:33 -0800

    Cleaned Day 2 Part 1 further

--- AdventOfCode/Day2p1.cs
+++ AdventOfCode/Day2p1.cs
@@ -7,24 +7,21 @@ namespace AdventOfCode
 {
     public class Day2p1
     {
-        record Data(int Low, int High, char C, string S);
-
         [Run(2, 1)]
         public static void Main(string input)
         {
-            var validPasswords = (from s
-                        in input.Split('\n')
+            var validPasswords = (from s in input.Split('\n')
                     select s.Split(' ')
                     into ss
                     let n12 = ss[0].Split('-')
-                    select (Data)
-                        new(int.Parse(n12[0]), int.Parse(n12[1]), ss[1].Replace(":", "")[0], ss[2].Replace("\r", "")))
+                    select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1].Replace(":", "")[0], ss[2].Replace("\r", "")))
                 .Count(d =>
                 {
-                    var r = new Regex($@"[^{d.C}]").Replace(d.S, "").Length;
-                    return d.Low <= r && d.High >= r;
+                    var (low, high, c, s) = d;
+                    var r = new Regex($@"[^{c}]").Replace(s, "").Length;
+                    return low <= r && high >= r;
                 });
-            
+
             Console.WriteLine($"{validPasswords}");
         }
     }
