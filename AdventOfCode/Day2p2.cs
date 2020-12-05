d80288a7d2ca0246421bbf6436b2ba8758a401a9
SW_Creeperking <sw.creeperking@gmail.com>
2020-12-04 16:28:53 -0800

    Cleaned Day 2 Part 2

--- AdventOfCode/Day2p2.cs
+++ AdventOfCode/Day2p2.cs
@@ -7,30 +7,24 @@ namespace AdventOfCode
 {
     public class Day2p2
     {
-        record Data(int Contain, int NotContain, char C, string S);
-
         [Run(2, 2)]
         public static void Main(string input)
         {
-            var inputs = (from s
-                    in input.Split('\n')
-                select s.Split(' ')
-                into ss
-                let n12 = ss[0].Split('-')
-                select (Data)
-                    new(int.Parse(n12[0]), int.Parse(n12[1]), ss[1].Replace(":", "")[0], ss[2])).ToList();
+            var inputs =
+                (from s in input.Split('\n')
+                    select s.Split(' ')
+                    into ss
+                    let n12 = ss[0].Split('-')
+                    select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1][0], ss[2].Replace("\r", ""))).Count(d =>
+                {
+                    var (contain, notContain, c, s) = d;
+                    if (!s.Contains(c) || s.Length < contain || s.Length < notContain) return false;
+                    var cked = s[contain - 1] == c;
+                    if (s[notContain - 1] == c) cked = !cked;
+                    return cked;
+                });
 
-            List<string> validPasswords = new();
-            
-            foreach (var (contain, notContain, c, s) in inputs)
-            {
-                if (!s.Contains(c) || s.Length < contain || s.Length < notContain) continue;
-                var cked = s[contain - 1] == c;
-                if (s[notContain - 1] == c) cked = !cked;
-                if (cked) validPasswords.Add(s);
-            }
-
-            Console.WriteLine($"{validPasswords.Count}");
+            Console.WriteLine($"{inputs}");
         }
     }
 }
