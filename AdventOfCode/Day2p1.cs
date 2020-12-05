f9178f6df75ab533f98cfa543c94fa4710610d04
SW_Creeperking <sw.creeperking@gmail.com>
2020-12-04 18:33:37 -0800

    Cleaned up day 2 part 2 again

--- AdventOfCode/Day2p1.cs
+++ AdventOfCode/Day2p1.cs
@@ -7,17 +7,15 @@ namespace AdventOfCode
     public class Day2p1
     {
         [Run(2, 1)]
-        public static int Main(string input) =>
-            (from s in input.Split('\n')
-                select s.Split(' ')
-                into ss
-                let n12 = ss[0].Split('-')
-                select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1][0], ss[2].Replace("\r", "")))
-            .Count(d =>
-            {
-                var (low, high, c, s) = d;
-                var r = new Regex($@"[^{c}]").Replace(s, "").Length;
-                return low <= r && high >= r;
-            });
+        public static int Main(string input) => (from s in input.Split('\n')
+            select s.Split(' ')
+            into ss
+            let n12 = ss[0].Split('-')
+            select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1][0], ss[2].Replace("\r", ""))).Count(d =>
+        {
+            var (low, high, c, s) = d;
+            var r = new Regex($@"[^{c}]").Replace(s, "").Length;
+            return low <= r && high >= r;
+        });
     }
 }
--- AdventOfCode/Day2p2.cs
+++ AdventOfCode/Day2p2.cs
@@ -6,18 +6,15 @@ namespace AdventOfCode
     public class Day2p2
     {
         [Run(2, 2)]
-        public static int Main(string input) =>
-            (from s in input.Split('\n')
-                select s.Split(' ')
-                into ss
-                let n12 = ss[0].Split('-')
-                select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1][0], ss[2].Replace("\r", ""))).Count(d =>
-            {
-                var (contain, notContain, c, s) = d;
-                if (!s.Contains(c) || s.Length < contain || s.Length < notContain) return false;
-                var cked = s[contain - 1] == c;
-                if (s[notContain - 1] == c) cked = !cked;
-                return cked;
-            });
+        public static int Main(string input) => (from s in input.Split('\n')
+            select s.Split(' ')
+            into ss
+            let n12 = ss[0].Split('-')
+            select (int.Parse(n12[0]), int.Parse(n12[1]), ss[1][0], ss[2].Replace("\r", ""))).Count(d =>
+        {
+            var (contain, notContain, c, s) = d;
+            if (!s.Contains(c) || s.Length < contain || s.Length < notContain) return false;
+            return s[contain - 1] == c ^ s[notContain - 1] == c;
+        });
     }
 }
