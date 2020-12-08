2896fbc1cfc896b56d285391f52b1c963c3e2b08
SW_Creeperking <sw.creeperking@gmail.com>
2020-12-07 22:13:39 -0800

    Fixed Day 4 part 2

--- AdventOfCode/Day4p2.cs
+++ AdventOfCode/Day4p2.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using AdventOfCode.Better_Run;
 
@@ -6,18 +7,29 @@ namespace AdventOfCode
 {
     public class Day4p2
     {
+        static Dictionary<string, string> regex = new()
+        {
+            {"byr", "19[2-9][0-9]|200[0-2]"},
+            {"iyr", "201[0-9]|2020"},
+            {"eyr", "202[0-9]|2030"},
+            {"hgt", "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in"},
+            {"hcl", "#[0-9a-f]{6}"},
+            {"ecl", "amb|blu|brn|gry|grn|hzl|oth"},
+            {"pid", "[0-9]{9}"},
+        };
+
         [Run(4, 2)]
-        public static int Main(string input) =>
-            (from s in new Regex(@"\n\n").Split(input)
-                where new Regex(@"[\n ]").Replace(s, "") != ""
-                select new Regex(@"[\n]").Replace(s, ""))
-            .Count(realS =>
-                new Regex(@"byr:(19[2-9][0-9]|200[0-2])").IsMatch(realS) &&
-                new Regex(@"iyr:20(1[0-9]|20)").IsMatch(realS) &&
-                new Regex(@"eyr:20(2[0-9]|30)").IsMatch(realS) &&
-                new Regex(@"hgt:(1([5-8][0-9]|9[0-3])cm|(50|6[0-9]|7[0-6])in)").IsMatch(realS) &&
-                new Regex(@"hcl:#[0-9a-f]{6}").IsMatch(realS) &&
-                new Regex(@"ecl:(amb|blu|brn|gry|grn|hzl|oth)").IsMatch(realS) &&
-                new Regex(@"pid:[0-9]{9}").IsMatch(realS));
+        public static int Main(string input)
+        {
+            return input.Split("\n\n").ToList().Select(s => s.Split("\n ".ToCharArray())).ToArray().Select(masterS =>
+                    masterS
+                        .ToList()
+                        .All(s =>
+                        {
+                            var split = s.Split(":");
+                            return !regex.ContainsKey(split[0]) || Regex.IsMatch(split[1], $"^({regex[split[0]]})$");
+                        }) && Regex.IsMatch(string.Join(" ", masterS), @"((byr|iyr|eyr|hgt|hcl|ecl|pid):.*){7}"))
+                .Count(doesCount => doesCount);
+        }
     }
 }
