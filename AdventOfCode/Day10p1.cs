208cb7f77ea7deb71847d04f39f87584f0d05e1a
SWCreeperKing <36772824+SWCreeperKing@users.noreply.github.com>
2020-12-09 22:33:15 -0800

    Day 10 part 1 and 2

---
+++ AdventOfCode/Day10p1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.Better_Run;
+
+namespace AdventOfCode
+{
+    public class Day10p1
+    {
+        [Run(10, 1)]
+        public static int Main(string input)
+        {
+            int diff1 = 0, diff3 = 1, last = 0;
+
+            foreach (var n in input.Split("\n").Select(int.Parse).OrderBy(i => i).ToArray())
+            {
+                switch (n - last)
+                {
+                    case 1:
+                        diff1++;
+                        break;
+                    case 3:
+                        diff3++;
+                        break;
+                    default:
+                        break;
+                }
+
+                last = n;
+            }
+
+            return diff1 * diff3;
+        }
+    }
+}
---
+++ AdventOfCode/Day10p2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Better_Run;
+
+namespace AdventOfCode
+{
+    public class Day10p2
+    {
+        [Run(10, 2)]
+        public static long Main(string input)
+        {
+            var numbers = new[] {0}.Concat(input.Split("\n").Select(int.Parse).OrderBy(i => i)).ToList();
+            numbers.Add(numbers.Max() + 3);
+            Dictionary<int, long> combos = new() {{numbers.Count - 2, 1}};
+
+            long Amass(int i = 0)
+            {
+                if (combos.ContainsKey(i)) return combos[i];
+
+                var adder = 0L;
+                for (var j = 1; j < 4; j++)
+                {
+                    var ij = i + j;
+                    if (ij < numbers.Count && numbers[ij] - numbers[i] < 4) adder += Amass(ij);
+                }
+
+                return combos[i] = adder;
+            }
+
+            Amass();
+            return combos[0];
+        }
+    }
+}
