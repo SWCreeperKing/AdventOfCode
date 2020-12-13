2772e3e1113b4eae50a6e393bf0155603cb230da
SW_CreeperKing <2jmphillip@gmail.com>
2020-12-12 18:20:37 -0800

    Push from 2-1

---
+++ AdventOfCode/Solutions/2015/Day3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AdventOfCode.Better_Run;
+
+namespace AdventOfCode.Solutions._2015
+{
+    public class Day3
+    {
+        [Run(2015, 3, 1)]
+        public static int Part1(string input)
+        {
+            Console.WriteLine(input);
+            return -1;
+        }
+    }
+}
