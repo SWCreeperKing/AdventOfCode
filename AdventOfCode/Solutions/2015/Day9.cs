58b92d56656aa3977be9c5e6b3aa6c4a191ce8d5
SW_CreeperKing <2jmphillip@gmail.com>
2020-12-14 10:23:35 -0800

    kek

---
+++ AdventOfCode/Solutions/2015/Day9.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Better_Run;
+
+namespace AdventOfCode.Solutions._2015
+{
+    public class Day9
+    {
+        [Run(2015, 9, 1)]
+        public static int Part1(string input)
+        {
+            return -1;
+        }
+
+        [Run(2015, 9, 2)]
+        public static int Part2(string input)
+        {
+            return -1;
+        }
+    }
+}
