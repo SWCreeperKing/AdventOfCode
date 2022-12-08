9c1475193e0f0c863c4c27b550c544d7cb7ef64e
SW_Creeperking <sw.creeperking@gmail.com>
2022-12-07 18:18:46 -0800

    2016 day 9

--- AdventOfCode/Solutions/2016/Day9.cs
+++ AdventOfCode/Solutions/2016/Day9.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using AdventOfCode.Experimental_Run;
 
@@ -9,42 +7,35 @@ namespace AdventOfCode.Solutions._2016;
 [Day(2016, 9, "Explosives in Cyberspace")]
 public static partial class Day9
 {
-    public static Regex paraFind = new(@"\((\d+)x(\d+)\)", RegexOptions.Compiled);
+    [GeneratedRegex(@"\((\d+)x(\d+)\)")] public static partial Regex ParaFind();
 
-    [Answer(120765)]
-    public static long Part1(string inp)
+    [Answer(120765)] public static long Part1(string inp) => CountData(inp);
+    [Answer(11658395076)] public static long Part2(string inp) => CountData(inp, true);
+
+    public static long CountData(string inp, bool recurse = false)
     {
         var str = inp.AsSpan();
-        StringBuilder sb = new();
+        long counter = 0;
 
         for (var i = 0; i < str.Length; i++)
         {
             if (str[i] != '(')
             {
-                sb.Append(str[i]);
+                counter++;
                 continue;
             }
 
-            i++;
-            var end = str[i..].IndexOf(')') + i;
-            var data = str[i..end];
-            var count = int.Parse(data[..data.IndexOf('x')]);
-            var repeat = int.Parse(data[(data.IndexOf('x') + 1)..]);
-            i += data.Length + 1;
-            var take = str[i..(i + count)];
-            i += count - 1;
-            sb.Append(Enumerable.Repeat(take.ToString(), repeat).Join());
+            var end = str[i..].IndexOf(')') + i + 1;
+            var data = ParaFind().Match(str[i..end].ToString()).Groups.Range(1..2).ToIntArr();
+            i = end + data[0] - 1;
+            if (recurse)
+            {
+                var subString = str[end..(end + data[0])].ToString();
+                counter += CountData(subString, recurse) * data[1];
+            }
+            else counter += data.Multi();
         }
 
-        return sb.ToString().Length;
+        return counter;
     }
-
-    // public static long Part2(string inp)
-    // {
-    //     inp = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN";
-    //     var str = inp;
-    //     while (str.Contains('(')) str = ComputeString(str);
-    //     return str.Length;
-    // }
-
 }
