b32e32c88026b26d9c4b0e0ef1448d1d3bbc1777
SW_Creeperking <sw.creeperking@gmail.com>
2022-12-09 23:18:30 -0800

    refactored 2022 day10

--- AdventOfCode/Helper.cs
+++ AdventOfCode/Helper.cs
@@ -15,6 +15,7 @@ public static class Helper
     public static char ToChar(this int i, int offset = 97) => (char) (i + offset);
     public static bool IsInRange(this int i, int min, int max) => min <= i && max >= i;
     public static bool IsInRange(this long l, int min, int max) => min <= l && max >= l;
+    public static bool IsInRange(this long l, long min, long max) => min <= l && max >= l;
     public static int[] ToIntArr(this IEnumerable<string> texts) => texts.Select(int.Parse).ToArray();
     public static long[] ToLongArr(this IEnumerable<string> texts) => texts.Select(long.Parse).ToArray();
     public static string ReplaceWithSpace(this string text, string pattern) => text.Replace(pattern, " ");
--- AdventOfCode/Solutions/2022/Day10.cs
+++ AdventOfCode/Solutions/2022/Day10.cs
@@ -1,101 +1,41 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using AdventOfCode.Experimental_Run;
 
 namespace AdventOfCode.Solutions._2022;
 
-[Day(2022, 10, "")]
+[Day(2022, 10, "Cathode-Ray Tube")]
 public class Day10
 {
     [ModifyInput] public static string[][] ProcessInput(string inp) => inp.SuperSplit('\n', ' ');
 
+    [Answer(13920)]
     public static long Part1(string[][] inp)
     {
-        long cycleAmount = 0;
-        long x = 1;
-        var cycle = 1;
-        Queue<int> iQueue = new();
-        int[] cycles = { 20, 60, 100, 140, 180, 220 };
-
+        long cycleAmount = 0, x = 1, cycle = 1;
+        Queue<int> numberQueue = new();
+        var cycles = new long[] { 20, 60, 100, 140, 180, 220 };
 
         for (var i = 0; i < inp.Length;)
         {
-            if (cycles.Contains(cycle))
-            {
-                cycleAmount += x * cycle;
-                Console.WriteLine($"{x} => {cycle}");
-            }
-
-            if (!iQueue.Any())
-            {
-                switch (inp[i])
-                {
-                    case ["noop"]:
-                        break;
-                    case ["addx", var amount]:
-                        iQueue.Enqueue(int.Parse(amount));
-                        break;
-                }
-
-                i++;
-            }
-            else
-            {
-                x += iQueue.Dequeue();
-            }
-
-            if (cycle >= 220) break;
-            cycle++;
-        }
-
-        return cycleAmount;
-    }
-
-    public static long Part2(string[][] inp)
-    {
-        long cycleAmount = 0;
-        long x = 1;
-        long crt = 1;
-        var cycle = 1;
-        Queue<int> iQueue = new();
-        int[] cycles = { 20, 60, 100, 140, 180, 220 };
-
-        for (var i = 0; i < inp.Length;)
-        {
-            Console.Write(crt >= x && crt <= x + 2 ? '#' : '.');
+            Console.Write((cycle % 40).IsInRange(x, x + 2) ? "██" : "  ");
             if (cycle % 40 == 0) Console.WriteLine();
-            
             if (cycles.Contains(cycle)) cycleAmount += x * cycle;
-
-            if (!iQueue.Any())
+            if (!numberQueue.Any())
             {
-                switch (inp[i])
-                {
-                    case ["noop"]:
-                        break;
-                    case ["addx", var amount]:
-                        iQueue.Enqueue(int.Parse(amount));
-                        break;
-                }
-
+                var line = inp[i];
+                if (line[0] is "addx") numberQueue.Enqueue(int.Parse(line[1]));
                 i++;
             }
-            else
-            {
-                x += iQueue.Dequeue();
-            }
+            else x += numberQueue.Dequeue();
 
             if (cycle >= 240) break;
             cycle++;
-            crt++;
-            crt %= 40;
         }
 
-        Console.WriteLine();
+        Console.WriteLine("\nPart 2 answer should be: EGLHBLFJ");
 
-        return 0;
+        return cycleAmount;
     }
 }
