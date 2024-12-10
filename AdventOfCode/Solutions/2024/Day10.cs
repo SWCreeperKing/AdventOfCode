b2af16a1b3717ec814cdccc1d25894f52d792e82
SWCreeperKing <SW.CreeperKing@gmail.com>
2024-12-10 12:10:41 -0800

    Day 10 improvement

--- AdventOfCode/Solutions/2024/Day10.cs
+++ AdventOfCode/Solutions/2024/Day10.cs
@@ -3,30 +3,32 @@ namespace AdventOfCode.Solutions._2024;
 [Day(2024, 10, "Hoof It")]
 file class Day10
 {
-    [ModifyInput] public static string ProcessInput(string input) => input;
+    [ModifyInput]
+    public static Matrix2d<int> ProcessInput(string input)
+    {
+        return new Matrix2d<int>(input.Split('\n')
+                                      .SelectArr(line => line.SelectArr(c => c == '.' ? 20 : int.Parse(c.ToString()))));
+    }
 
     [Answer(510)]
-    public static long Part1(string inp)
+    public static long Part1(Matrix2d<int> inp)
     {
-        var nlInp = inp.Split('\n');
-        Matrix2d<int> map = new(nlInp.SelectArr(line => line.SelectArr(c => c == '.' ? 20 : int.Parse(c.ToString()))));
-        var highes = map.FindAll(i => i == 9);
-        var lowes = map.FindAll(i => i == 0);
+        var highes = inp.FindAll(i => i == 9);
+        var lowes = inp.FindAll(i => i == 0);
 
         var sum = 0;
         Parallel.ForEach(lowes, low =>
         {
-
             foreach (var high in highes)
             {
-                if (!Path([low], low, high, 0)) continue;
+                if (!Path(low, high, 0)) continue;
                 Increment(ref sum);
             }
         });
 
         return sum;
 
-        bool Path(Pos[] path, Pos start, Pos end, int step)
+        bool Path(Pos start, Pos end, int step)
         {
             if (start == end && step == 9) return true;
             if (step > 9) return false;
@@ -34,10 +36,9 @@ file class Day10
             foreach (var dir in Pos.Surround)
             {
                 var next = start + dir;
-                if (!map.PositionExists(next)) continue;
-                if (path.Contains(next)) continue;
-                if (Math.Abs(map[start] - map[next]) > 1) continue;
-                if (!Path([..path, next], next, end, step + 1)) continue;
+                if (!inp.PositionExists(next)) continue;
+                if (inp[next] - inp[start] < 1) continue;
+                if (!Path(next, end, step + 1)) continue;
                 return true;
             }
 
@@ -46,26 +47,24 @@ file class Day10
     }
 
     [Answer(1058)]
-    public static long Part2(string inp)
+    public static long Part2(Matrix2d<int> inp)
     {
-        var nlInp = inp.Split('\n');
-        Matrix2d<int> map = new(nlInp.SelectArr(line => line.SelectArr(c => c == '.' ? 20 : int.Parse(c.ToString()))));
-        var highes = map.FindAll(i => i == 9);
-        var lowes = map.FindAll(i => i == 0);
+        var highes = inp.FindAll(i => i == 9);
+        var lowes = inp.FindAll(i => i == 0);
 
         var sum = 0;
-        
+
         Parallel.ForEach(lowes, low =>
         {
             foreach (var high in highes)
             {
-                Add(ref sum, Path([low], low, high, 0));
+                Add(ref sum, Path(low, high, 0));
             }
         });
 
         return sum;
 
-        int Path(Pos[] path, Pos start, Pos end, int step)
+        int Path(Pos start, Pos end, int step)
         {
             if (start == end && step == 9) return 1;
             if (step > 9) return 0;
@@ -74,10 +73,9 @@ file class Day10
             foreach (var dir in Pos.Surround)
             {
                 var next = start + dir;
-                if (!map.PositionExists(next)) continue;
-                if (path.Contains(next)) continue;
-                if (Math.Abs(map[start] - map[next]) > 1) continue;
-                summ += Path([..path, next], next, end, step + 1);
+                if (!inp.PositionExists(next)) continue;
+                if (inp[next] - inp[start] < 1) continue;
+                summ += Path(next, end, step + 1);
             }
 
             return summ;
